6ba8d2c9fe400589c49f56decabd007cd81cdb2c
Lee Shiueh <lee.shiueh@gmail.com>
2019-04-03 23:08:21 +0800

    update

--- invsha3_cmd.sql
+++ invsha3_cmd.sql
@@ -1,2 +1,2 @@
-select a.StockID, a.InvSha3, b.InvSha3, a.InvSha3+b.InvSha3 as TotalSha3 from twnforinvtrad_chip a, twnfundtrad_chip b where a.StockID = b.StockID order by TotalSha3 desc limit 30 INTO OUTFILE '/Users/lee_shiueh/ws/yahoo_quote/src/data.csv' FIELDS TERMINATED BY ',' ENCLOSED BY '"' LINES TERMINATED BY '\n';
+select a.StockID, a.InvSha3, b.InvSha3, a.InvSha3+b.InvSha3 as TotalSha3 from twnforinvtrad_chip a, twnfundtrad_chip b where a.StockID = b.StockID order by TotalSha3 desc limit 30;
 
